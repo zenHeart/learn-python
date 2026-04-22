df8b43cc1301fc3527dbe9a7365648f783e95356
zenHeart <zenheart_register@163.com>
2026-04-22 01:31:11 +0000

    Deploying to gh-pages from @ zenHeart/learn-python@fc99b7fd22236e8b164c0e722690e3923424a899 🚀

---
+++ assets/progress.md.CEg5IQpo.lean.js
@@ -0,0 +1,5 @@
+import{_ as e,o as r,c as o,ae as a,j as t}from"./chunks/framework.DJK_dC0-.js";const y=JSON.parse('{"title":"学习进度","description":"","frontmatter":{},"headers":[],"relativePath":"progress.md","filePath":"progress.md"}'),h={name:"progress.md"};function n(l,d,i,s,b,c){return r(),o("div",null,[...d[0]||(d[0]=[a("",19),t("div",{class:"py-demo","data-code":"%23%20%E7%AC%AC%E4%B8%80%E4%B8%AA%20Python%20%E7%A8%8B%E5%BA%8F%0Aname%20%3D%20%22Python%22%0Aprint(f%22Hello%2C%20%7Bname%7D!%22)%0Aprint(%22Welcome%20to%20learn-python!%22)%0A"},[t("div",{class:"py-demo-header"},[t("span",null,"Python"),t("button",{class:"py-demo-run",onclick:"window.__runPyDemo(this)"},"Run")]),t("pre",{class:"language-python"},[t("code",null,`# 第一个 Python 程序
+name = "Python"
+print(f"Hello, {name}!")
+print("Welcome to learn-python!")
+`)]),t("div",{class:"py-demo-output"},"Click Run to see output")],-1),a("",11)])])}const u=e(h,[["render",n]]);export{y as __pageData,u as default};
